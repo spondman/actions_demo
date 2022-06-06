fcf5eca0b5a41d5cbd98db1f5e1d577859d7b561
Cong Huang <cong.huang@compass.com>
2022-06-06 12:33:06 +0800

    init

---
+++ actions_demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  actions_demo
+//
+//  Created by eden on 2022/6/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ actions_demo/actions_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  actions_demoApp.swift
+//  actions_demo
+//
+//  Created by eden on 2022/6/6.
+//
+
+import SwiftUI
+
+@main
+struct actions_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
